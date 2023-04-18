9cb0bd034399b7ffa5179b82847ccc2ab7088acd
Michael Fagan <michael.g.fagan@gmail.com>
2023-04-18 08:53:28 -0500

    Make calculator function

--- script.js
+++ script.js
@@ -1,16 +1,70 @@
 let numA = 0;
 let numB = 0;
-let operator = 'add';
-let displayNumber = '';
+let operator = '';
+let displayNumber = '0';
+let isOperatorActive = false;
+let isResetActive = true;
 
 display = document.querySelector('#display');
 
-const buttons = document.querySelectorAll('.number');
-buttons.forEach((button) => {
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        displayNumber = displayNumber + e.target.textContent;
-        updateDisplay(displayNumber);
+        inputNumber = e.target.textContent;
+        if (!(displayNumber === "0" && inputNumber === "0")) {
+            if (isResetActive) {
+                isResetActive = false;
+                displayNumber = '';
+            };
+            if (isOperatorActive) {
+                isOperatorActive = false;
+            };
+            displayNumber = displayNumber + inputNumber;
+            updateDisplay(displayNumber);
+        }
     });
+});
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        symbol = e.target.textContent
+        if (symbol === '+') {
+            operator = 'add';
+        }
+        else if (symbol === '-') {
+            operator = 'subtract';
+        }
+        else if (symbol === '*') {
+            operator = 'multiply';
+        }
+        else if (symbol === '/') {
+            operator = 'divide';
+        }
+        numA = Number(displayNumber);
+        isOperatorActive = true;
+        isResetActive = true;
+    })
+});
+
+const equalButton = document.querySelector('#equals');
+equalButton.addEventListener('click', (e) => {
+    if (operator) {
+        numB = Number(displayNumber);
+        displayNumber = operate(numA, numB, operator);
+        updateDisplay(displayNumber);
+    };
+});
+
+const eraseButton = document.querySelector('#erase');
+eraseButton.addEventListener('click', (e) => {
+    numA = 0;
+    numB = 0;
+    operator = '';
+    displayNumber = '0';
+    isOperatorActive = false;
+    isResetActive = true;
+    updateDisplay(displayNumber);
 });
 
 function updateDisplay(displayNumber) {
