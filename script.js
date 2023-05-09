3e28b1b1da77b6b2e893e87ea25de421963249cb
Michael Fagan <michael.g.fagan@gmail.com>
2023-05-09 08:06:37 -0500

    Add keyboard support

--- script.js
+++ script.js
@@ -7,48 +7,56 @@ let isOperatorActive = false;
 
 display = document.querySelector('#display');
 
+window.addEventListener('keydown', handleKeyboardInput)
+
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
         inputNumber = e.target.textContent;
-        if (!(displayNumber === "0" && inputNumber === "0")) {
-            buttonInput(inputNumber);
-            isOperatorActive = false;
-        }
+        buttonInput(inputNumber);
     });
 });
 
 const decimalButton = document.querySelector('.decimal');
 decimalButton.addEventListener('click', (e) => {
-    if (!displayNumber.includes('.')) {
-        buttonInput('.');
-        isOperatorActive = false;
-    }
+    buttonInput('.');
 });
 
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
         symbol = e.target.textContent;
-        if (!isOperatorActive) {
-            if (operator) {
-                numB = Number(displayNumber);
-                runningTotal = operate(runningTotal, numB, operator);
-                displayNumber = runningTotal;
-                updateDisplay(displayNumber);
-            }
-            else {
-                runningTotal = Number(displayNumber);
-            };
-        }
         setOperator(symbol);
-        isResetActive = true;
-        isOperatorActive = true;
     })
 });
 
 const equalButton = document.querySelector('#equals');
 equalButton.addEventListener('click', (e) => {
+    clickEquals();
+});
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', (e) => {
+    resetDisplay();
+});
+
+const backspaceButton = document.querySelector('#backspace');
+backspaceButton.addEventListener('click', (e) => {
+    clickBackspace();
+});
+
+
+function handleKeyboardInput(e) {
+    console.log(e.key);
+    if (e.key >= 0 && e.key <= 9 && e.key != ' ') buttonInput(e.key)
+    if (e.key === '.') buttonInput(e.key)
+    if (e.key === '=' || e.key === 'Enter') clickEquals()
+    if (e.key === 'Backspace') clickBackspace()
+    if (e.key === 'Escape') resetDisplay()
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') setOperator(e.key)
+};
+
+function clickEquals() {
     if (operator && !isResetActive) {
         numB = Number(displayNumber);
         runningTotal = operate(runningTotal, numB, operator);
@@ -57,37 +65,44 @@ equalButton.addEventListener('click', (e) => {
         operator = '';
         isResetActive = true;
     };
-});
+};
 
-const clearButton = document.querySelector('#clear');
-clearButton.addEventListener('click', (e) => {
-    resetDisplay();
-    isOperatorActive = false;
-});
-
-const backspaceButton = document.querySelector('#backspace');
-backspaceButton.addEventListener('click', (e) => {
+function clickBackspace() {
     if (!isResetActive) {
         if (displayNumber.length === 1) {
-            updateDisplay('0');
+            displayNumber = 0;
+            isResetActive = true;
         }
         else {
             displayNumber = displayNumber.slice(0, -1)
-            updateDisplay(displayNumber);
         }
+        updateDisplay('0');
     };
-});
+};
 
 function buttonInput(inputNumber) {
+    if ( !(displayNumber === "0" && inputNumber === "0") || (!displayNumber.includes('.') && inputNumber === '.') )
     if (isResetActive) {
         isResetActive = false;
         displayNumber = '';
     };
     displayNumber = displayNumber + inputNumber;
     updateDisplay(displayNumber);
+    isOperatorActive = false;
 };
 
 function setOperator(symbol) {
+    if (!isOperatorActive) {
+        if (operator) {
+            numB = Number(displayNumber);
+            runningTotal = operate(runningTotal, numB, operator);
+            displayNumber = runningTotal;
+            updateDisplay(displayNumber);
+        }
+        else {
+            runningTotal = Number(displayNumber);
+        };
+    }
     if (symbol === '+') {
         operator = 'add';
     }
@@ -100,6 +115,8 @@ function setOperator(symbol) {
     else if (symbol === '/') {
         operator = 'divide';
     }
+    isResetActive = true;
+    isOperatorActive = true;
 };
 
 function resetDisplay() {
